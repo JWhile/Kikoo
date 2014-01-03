1ffc2153ae52bd27e8d51cd313098e3618d6f66c
JWhile <juloo.dsi@gmail.com>
2014-01-03 13:30:11 +0100

    Animation changement d'app + mini fix

--- main.js
+++ main.js
@@ -7,14 +7,41 @@
 // class Kikoo
 function Kikoo()
 {
-    this.main = new MainApp().insert(document.body);
-    this.cookie = new CookieApp().insert(document.body);
+    this.scroll = new Builder('div')
+        .className('scroll')
+        .insert(document.body);
+
+    this.apps = [
+        new MainApp().insert(this.scroll),
+        new CookieApp().insert(this.scroll)
+    ];
+
+    this.move(0);
 }
+// function move(int index):void
+Kikoo.prototype.move = function(index)
+{
+    this.apps[index].css('display', '');
+
+    this.scroll.css('left', '-'+ (index * 210) +'px');
+
+    var self = this;
+
+    setTimeout(function()
+    {
+        for(var i = 0; i < self.apps.length; ++i)
+        {
+            self.apps[i].css('display', (i === index)? '' : 'none');
+        }
+    }, 340);
+};
 
 // class MainApp extends Builder
-function MainApp()
+function MainApp(main)
 {
     this.super('div');
+
+    this.main = main;
 
     this.set('id', 'main')
         .className('app');
@@ -28,8 +55,12 @@ function MainApp()
 
     this.content = new Builder('div')
         .className('content')
-        .append(new Builder('a')
-            )
+        .append(new Builder('p')
+            .html('<b>Bienvenue kikoo !</b><br /><i>Choisi une application</i>'))
+        .append(new Builder('p')
+            .append(new Builder('a')
+                .text('Cookie'))
+            .add('<br /><i>Modifie tes cookies comme un kikoo !</i>'))
         .insert(this);
 
     new Builder('div')
