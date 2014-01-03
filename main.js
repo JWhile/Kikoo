22652617c5f51a54ba9e575a0a4bdb4ef3061d16
JWhile <juloo.dsi@gmail.com>
2014-01-03 12:12:53 +0100

    Ajouter MainApp

--- main.js
+++ main.js
@@ -4,6 +4,23 @@
  * main.js
  */
 
-var kikoo = new CookieApp();
+// class Kikoo
+function Kikoo()
+{
+    this.main = new MainApp();
+    this.cookie = new CookieApp();
+}
+
+// class MainApp extends Builder
+function MainApp()
+{
+    this.super('div');
+
+    this.set('id', 'main')
+        .className('app');
+}
+fus.extend(MainApp, Builder);
+
+var kikoo = new Kikoo();
 
 kikoo.load();
