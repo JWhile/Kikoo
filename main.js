2b8556c09acf3f7e9bd6ee174a975f82113ac38b
JWhile <juloo.dsi@gmail.com>
2014-01-03 12:34:00 +0100

    Organise main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@
 // class Kikoo
 function Kikoo()
 {
-    this.main = new MainApp();
-    this.cookie = new CookieApp();
+    this.main = new MainApp().inser(document.body);
+    this.cookie = new CookieApp().insert(document.body);
 }
 
 // class MainApp extends Builder
@@ -18,9 +18,25 @@ function MainApp()
 
     this.set('id', 'main')
         .className('app');
+
+    new Builder('div')
+        .className('head')
+        .append(new Builder('p')
+            .className('logo')
+            .html('<b>Kikoo</b>! <span>...</span>'))
+        .insert(this);
+
+    this.content = new Builder('div')
+        .className('content')
+        .append(new Builder('a')
+            )
+        .insert(this);
+
+    new Builder('div')
+        .className('foot')
+        .html('By <a href="https://github.com/JWhile">juloo</a>')
+        .insert(this);
 }
 fus.extend(MainApp, Builder);
 
 var kikoo = new Kikoo();
-
-kikoo.load();
